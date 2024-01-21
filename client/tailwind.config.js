d75de04f87bcfad93e1ce310560339657bab7092
Saksham Mathur <saksham.mathur25@gmail.com>
2024-01-22 02:56:46 +0530

    tailwind and postcss config update

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,19 +1,12 @@
-module.exports = {
-  purge: [
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
     "./index.html",
-        "./src/**/*.{js,ts,jsx,tsx}",
-        "./node_modules/flowbite/**/*.js",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js",
   ],
-  // darkMode: media, // or 'media' or 'class'
   theme: {
     extend: {},
   },
-  variants: {
-    extend: {},
-  },
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-    require('flowbite/plugin')
-  ],
-}
+  plugins: [require("flowbite/plugin")],
+};
