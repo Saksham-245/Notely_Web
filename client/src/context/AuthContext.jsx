3d86d56f31c688e44d10679b43c2b55a69a9475e
Saksham Mathur <saksham.mathur25@gmail.com>
2024-01-22 02:42:04 +0530

    added checkAuth state to check for authentication states.

--- client/src/context/AuthContext.jsx
+++ client/src/context/AuthContext.jsx
@@ -1,5 +1,6 @@
-import { createContext, useMemo, useState } from 'react';
+import { createContext, useCallback, useEffect, useMemo, useState } from 'react';
 import PropTypes from 'prop-types';
+import Cookies from 'js-cookie';
 
 // Create a new context for authentication
 export const AuthContext = createContext();
@@ -19,6 +20,24 @@ export const AuthProvider = ({ children }) => {
     setUser(null);
   };
 
+  // Define a function to check authentication status
+  const checkAuth = useCallback(async () => {
+    try {
+      const userCookie = Cookies.get('user');
+      if (userCookie) {
+        const userData = JSON.parse(userCookie);
+        saveUser(userData);
+      }
+    } catch (error) {
+      console.error('Failed to authenticate user', error);
+    }
+  }, []);
+
+  // Call checkAuth to set user data if available
+  useEffect(() => {
+    checkAuth();
+  }, [checkAuth]);
+
   const value = useMemo(() => ({ user, saveUser, unsetUser }), [user]);
 
   // Provide the user's information and the login and logout functions to the children components
@@ -30,5 +49,5 @@ export const AuthProvider = ({ children }) => {
 };
 
 AuthProvider.propTypes = {
-    children: PropTypes.node
+  children: PropTypes.node
 }
